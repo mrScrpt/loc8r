d7cdc3f47f27551f705cb259809f6f952b1bee61
mrScrpt <dolcegabbana2010@gmail.com>
2019-02-11 21:54:24 +0200

    add template views

--- app_server/controllers/locations.js
+++ app_server/controllers/locations.js
@@ -1,11 +1,11 @@
 module.exports.homeList = (req, res)=>{
-  res.render('index', {title: 'Home'})
+  res.render('locations-list', {title: 'Home'})
 };
 
 module.exports.locationInfo = (req, res)=>{
-  res.render('index', {title: 'location Info'})
+  res.render('locations-info', {title: 'location Info'})
 };
 
 module.exports.addReview = (req, res)=>{
-  res.render('index', {title: 'Add Review'})
+  res.render('location-review-form', {title: 'Add Review'})
 };
--- app_server/controllers/other.js
+++ app_server/controllers/other.js
@@ -1,3 +1,3 @@
 module.exports.about = (req,res)=>{
-    res.render('index', {title: 'About'});
+    res.render('generic-text', {title: 'About'});
 };
