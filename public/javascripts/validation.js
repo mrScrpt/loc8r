feb60fd136fec02c74b954ac71b285f3231a0cf2
mrScrpt <dolcegabbana2010@gmail.com>
2019-02-25 18:38:45 +0200

    chapter 8 angular start

--- public/javascripts/validation.js
+++ public/javascripts/validation.js
@@ -1,17 +1,20 @@
 window.addEventListener('DOMContentLoaded',()=>{
   const form = document.querySelector('#addReviewForm');
-  const labels = form.querySelectorAll('.validate-fields');
+  if(form){
+    const labels = form.querySelectorAll('.validate-fields');
 
-  form.addEventListener('submit', (e)=>{
-    for (let i = 0; i < labels.length; i++){
-      if(!labels[i].value){
-        e.preventDefault();
-        labels[i].classList.add('validate-error');
-      }else {
-        if(labels[i].classList.contains('validate-error')){
-          labels[i].classList.remove('validate-error');
+    form.addEventListener('submit', (e)=>{
+      for (let i = 0; i < labels.length; i++){
+        if(!labels[i].value){
+          e.preventDefault();
+          labels[i].classList.add('validate-error');
+        }else {
+          if(labels[i].classList.contains('validate-error')){
+            labels[i].classList.remove('validate-error');
+          }
         }
       }
-    }
-  })
+    })
+  }
+
 });
